16680262db825e0bb704d9794b9857bdc9262614
rorg314 <rorywhybrow@gmail.com>
2021-11-30 14:36:35 +0000

    App component scafolding

--- balances/src/App.js
+++ balances/src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
-import './App.css';
+
+import { Tabs, Tab } from 'react-bootstrap'
+import React, { Component } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='text-monospace'>
+        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
+          
+        <img src={logo} className="App-logo" alt="logo" height="32"/>
+          <b>CryptoDashboard</b>
+        
+        </nav>
+        <div className="container-fluid mt-5 text-center">
+        <br></br>
+          <h1>CryptoBalanceDashboard</h1>
+          <h2></h2>
+          <br></br>
+          <div className="row">
+            <main role="main" className="col-lg-12 d-flex text-center">
+              <div className="content mr-auto ml-auto">
+              <Tabs defaultActiveKey="profile" id="mainTabs">
+                
+                <Tab eventKey="dashboard" title="dashboard">
+                  <div>
+                      <h1>Hi</h1>
+                  </div>
+                </Tab>
+                
+              </Tabs>
+              </div>
+            </main>
+          </div>
+        </div>
+      </div>
   );
 }
 
