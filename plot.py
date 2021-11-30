5f164810879dd9509e36a2fb82de07fc816cda15
rorg314 <rorywhybrow@gmail.com>
2021-11-30 14:05:19 +0000

    gitignore

--- plot.py
+++ plot.py
@@ -5,7 +5,7 @@ from coinbaseReport import *
 def PlotBuys(reportData:ReportData, currency:str, ax=None):
     buyData = reportData.buyData[currency]
 
-    timestamps = buyData['Timestamp']
+    timestamps = [mktime(time.timetuple()) for time in buyData['Timestamp']]
     buyUSD = buyData['Total (inclusive of fees)']
 
     if(not ax):
@@ -20,7 +20,7 @@ def PlotBuys(reportData:ReportData, currency:str, ax=None):
 # Cumulative price over time (spot price)
 def PlotCumSpotPrice(priceData, currency:str, ax=None):
 
-    timestamps = priceData['Timestamp']
+    timestamps = [mktime(time.timetuple()) for time in priceData['Timestamp']]
     buyUSD = priceData['Total (inclusive of fees)'].to_list()
     cumUSD = [sum(buyUSD[0:x:1]) for x in range(0, len(buyUSD))]
 
@@ -42,7 +42,7 @@ def PlotActualCurrencyPrice(priceData, currency:str, ax=None):
     cumAmount = [sum(amounts[0:x:1]) for x in range(0, len(amounts))]
 
     realSpotPrices = [cryptocompare.get_historical_price(currency, 'USD', time)[currency]['USD'] for time in unixTimes]
-    realPrices = [a*b for a, b in zip(cumAmount,realSpotPrices)]
+    realPrices = [a*b for a, b in zip(cumAmount, realSpotPrices)]
     
     if(not ax):
         fix, ax = plt.subplots()
