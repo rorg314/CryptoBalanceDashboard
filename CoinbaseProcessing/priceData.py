f2b5a1131af9728e421ec67745c783244eedd727
Rory Whybrow <rorywhybrow@gmail.com>
2022-10-02 21:50:22 +0100

    Using manually concatenated price data (TODO: use api from new repo)

--- CoinbaseProcessing/priceData.py
+++ CoinbaseProcessing/priceData.py
@@ -3,8 +3,6 @@ import pandas as pd
 import requests
 import json
 import time
-
-
 from config import *
 
 def CheckLastFetch(symbol):
@@ -23,27 +21,32 @@ def CheckLastFetch(symbol):
             f.write('0')
             return True
 
-def FetchDailyData(symbol):
-    pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/EUR
-    symbol = pair_split[0] + '-' + pair_split[1]
-    url = f'https://api.pro.coinbase.com/products/{symbol}/candles?granularity=86400'
-    response = requests.get(url)
-    if response.status_code == 200:  # check to make sure the response from server is good
-        data = pd.DataFrame(json.loads(response.text), columns=['unix', 'low', 'high', 'open', 'close', 'volume'])
-        # Reverse the dataframe so it goes from last -> today date 
-        data = data.iloc[::-1]
-        data['date'] = pd.to_datetime(data['unix'], unit='s')  # convert to a readable date
-        data['vol_fiat'] = data['volume'] * data['close']      # multiply the BTC volume by closing price to approximate fiat volume
 
-        # if we failed to get any data, print an error...otherwise write the file
-        if data is None:
-            print("Did not return any data from Coinbase for this symbol")
-        else:
-            data.to_csv(ROOTPATH + f'/CoinbaseProcessing/Prices/Coinbase_{pair_split[0] + pair_split[1]}_dailydata.csv', index=False)
-            with open (PRICES_FOLDER +symbol+ r"_lastfetch.txt", 'w+') as f:
-                lastFetch = f.write(str(time.time()))
-    else:
-        print("Did not receieve OK response from Coinbase API")
+def FetchDailyData(symbol):
+
+    # TODO: Replace properly with better api implementation - for now just bypass the fetch and use the cached data only
+    return
+
+    # pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/EUR
+    # symbol = pair_split[0] + '-' + pair_split[1]
+    # url = f'https://api.pro.coinbase.com/products/{symbol}/candles?granularity=86400'
+    # response = requests.get(url)
+    # if response.status_code == 200:  # check to make sure the response from server is good
+    #     data = pd.DataFrame(json.loads(response.text), columns=['unix', 'low', 'high', 'open', 'close', 'volume'])
+    #     # Reverse the dataframe so it goes from last -> today date 
+    #     data = data.iloc[::-1]
+    #     data['date'] = pd.to_datetime(data['unix'], unit='s')  # convert to a readable date
+    #     data['vol_fiat'] = data['volume'] * data['close']      # multiply the BTC volume by closing price to approximate fiat volume
+
+    #     # if we failed to get any data, print an error...otherwise write the file
+    #     if data is None:
+    #         print("Did not return any data from Coinbase for this symbol")
+    #     else:
+    #         data.to_csv(ROOTPATH + f'/CoinbaseProcessing/Prices/Coinbase_{pair_split[0] + pair_split[1]}_dailydata.csv', index=False)
+    #         with open (PRICES_FOLDER +symbol+ r"_lastfetch.txt", 'w+') as f:
+    #             lastFetch = f.write(str(time.time()))
+    # else:
+    #     print("Did not receieve OK response from Coinbase API")
 
 def JSONPriceData(symbol):
     pair_split = symbol.split('/')  # symbol must be in format XXX/XXX ie. BTC/EUR
@@ -56,7 +59,6 @@ def JSONPriceData(symbol):
     with open(outPath, 'w+') as f:
         f.write(JSON)
     
-
 
 def FetchCachedPriceData(pairs):
 
